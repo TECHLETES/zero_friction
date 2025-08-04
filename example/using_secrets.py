9fc6148b56ded404c672af92535d85c750a11f2d
Thom Ezechiels <thom@techletes.ai>
2025-08-04 15:13:35 +0200

    Add auto-formatting hooks for Python code using Black and isort

--- example/using_secrets.py
+++ example/using_secrets.py
@@ -1,10 +1,9 @@
-
 from utils.secrets import get_secret
 
-# Use the get_secret function to retrieve your secret. 
+# Use the get_secret function to retrieve your secret.
 # To find the path to your secret go to 1Password Interace > your item > click dropdown next to field > "Copy secret reference"
 api_key = get_secret("op://Shared with all/RAGFLow API Key/credential")
 if api_key:
     print(f"API_KEY is set: {api_key}")
 else:
-    print("API_KEY is not set.")
+    print("API_KEY is not set.")  # test
