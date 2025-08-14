878d325269da28d906c419d8f948a5a0d2c3adc9
Roman <roman@techletes.ai>
2025-08-14 16:19:30 +0200

    delete testing files not necessary for template

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-
-from utils.secrets import get_secret
-
-API_KEY = get_secret("op://Vaanster/Vaanster ZeroFriction Standard API/credential")
-
-
-if __name__ == "__main__":
-    print(f"testing secret retrieval: {API_KEY}")  # Bad indentation
